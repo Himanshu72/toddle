c4c59b72be58473003f3b02a1035aa39d4451847
Himanshu_Joshii <202012094@daiict.ac.in>
2021-08-21 22:47:06 +0530

    assignment edpoint

--- Assignment/DB/utility.js
+++ Assignment/DB/utility.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { obj } = require("../DB/Assignment");
 const Assignment=require("../DB/Assignment");
 const Submit=require("../DB/Submit");
 const env=require("../env");
@@ -18,7 +19,7 @@ mongoose.connect(env.dbserver, {
    module.exports={
     insertAssignment:async (obj)=>{
         try{
-            
+            console.log(obj);
           const AssignmentData=new AssignmentModel(obj);
           const data= await AssignmentData.save();
           return data;
@@ -26,5 +27,38 @@ mongoose.connect(env.dbserver, {
             console.log(e);
             throw e;
         }
-}
+    },
+    updateAssignment:async(obj)=>{
+        try{ 
+        let data=await AssignmentModel.findOneAndUpdate({_id:obj._id},obj,{new:true});
+        return data;
+        }catch(e){
+            console.log(e);
+            throw e;
+        }
+    },
+    deleteAssignment:async(obj)=>{
+          try{
+                let data=await AssignmentModel.deleteOne({_id:obj._id});
+                console.log(data);
+                return data;
+          }catch(e){
+              throw e;
+          } 
+    },getAllTeacherAssignment:async(obj)=>{
+        try{
+         let data=await AssignmentModel.find({createdBy:obj.username});
+            return data;
+        }catch(e){
+            throw e;
+        }
+    },
+    getStudentAssignment:async(obj)=>{
+        try{
+            let data=await AssignmentModel.find({ students:{$all:[obj.username]} });
+            return data;
+        } catch(e){ 
+            throw e;
+        }  
+    }
    }
--- Assignment/graphQL/schema.js
+++ Assignment/graphQL/schema.js
@@ -2,22 +2,105 @@ var { graphql, buildSchema } = require('graphql');
 const utility=require('../DB/utility');
 let schema = buildSchema(`
   type Query {
-        createAssignment:Assignment
+        createAssignment(publishAt:String!,description:String!,deadLine:String!,createdBy:String!,students:[String]!):Assignment
+        updateAssignment(_id:String!,description:String,deadLine:String,createdBy:String,students:[String]):Assignment
+        deleteAssignment(_id:String!):Message
+        getAssignment(filter:String,username:String!,isStudent:Boolean!):[Assignment]
     }
    type Assignment{
       description:String
       students:[String]
-      postedAt:String
+      publishAT:String
       deadLine:String
       createdBy:String 
+      status:String
+      _id:String
    } 
+
+   type Message{
+       message:String
+   }
 
 `);
 
+function getAssignmentStatus(publishAt,deadLine){
+    let curDate=new Date().getTime();
+    publishAt= publishAt.getTime();
+    deadLine=deadLine.getTime();
+    
+    if(curDate >publishAt ){
+        return "ONGOING"
+   }else{
+        return "SHEDULED"
+   }
+}
+
 let resolver = { 
-                    createAssignment:()=>{
-                            return "hello"
-                    }
+                     createAssignment:async (args)=>{
+                        try{
+                            args.pusblishAt=new Date(args.publishAt);
+                            args.deadLine=new Date(args.deadLine);
+                        let data=await utility.insertAssignment(args);
+                        data.status=getAssignmentStatus(data.publishAt,data.deadLine);
+                        return data;
+                        }catch(e){
+                            console.log(e);
+                            throw e;
+                        }
+                    },
+                    updateAssignment:async (args)=>{
+                        try{
+                        let data=await utility.updateAssignment(args);
+                        
+                        data.status=getAssignmentStatus(data.publishAt,data.deadLine);
+                        return data;
+                        }catch(e){
+                            throw e;
+                        }
+                    },
+                    deleteAssignment: async(args)=>{
+                        try{
+                            let data=await utility.deleteAssignment(args);
+                            if(data.deletedCount==1)
+                                    return {message:"Sucessfully Deleted"}
+                            return {message:"Invalid ID"};
+                        }catch(e){
+                            throw e;
+                        }
+                    },
+                    getAssignment:async (args)=>{
+                            if(args.isStudent){
+                                try{
+                                    let data=await utility.getStudentAssignment(args);
+                                    for(let a of data){
+                                        a.status=getAssignmentStatus(a.publishAt,a.deadLine);
+                                    }
+                                    if(args.filter){
+                                        data=data.filter( (cur)=>{
+                                            return cur.status==args.filter;
+                                        })
+                                    }
+                                    return data;
+                                }catch(e){
+                                    throw e;
+                                }
+                            }else{
+                              try{
+                                     let data=await utility.getAllTeacherAssignment(args);
+                                     for(let a of data){
+                                        a.status=getAssignmentStatus(a.publishAt,a.deadLine);
+                                        }
+                                        if(args.filter){
+                                            data=data.filter( (cur)=>{
+                                                return cur.status==args.filter;
+                                            })
+                                        }
+                                     return data;
+                                }catch(e){
+                                    throw e;
+                                }
+                            }
+                  }
                 };
 module.exports={
     schema:schema,
