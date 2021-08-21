d21c53a9f8cff7dae9a6e483743166d42fa75d97
Himanshu_Joshii <202012094@daiict.ac.in>
2021-08-21 21:14:42 +0530

    CRUD for assignment

--- DB/utility.js
+++ DB/utility.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { obj } = require("../DB/Assignment");
 const Assignment=require("../DB/Assignment");
 const Submit=require("../DB/Submit");
 const env=require("../env");
@@ -26,5 +27,30 @@ mongoose.connect(env.dbserver, {
             console.log(e);
             throw e;
         }
-}
+    },
+    updateAssignment:async(obj)=>{
+        try{ 
+        let data=await AssignmentModel.findOneAndUpdate({_id:obj._id},obj,{new:true});
+        return data;
+        }catch(e){
+            console.log(e);
+            throw e;
+        }
+    },
+    deleteAssignment:async(obj)=>{
+          try{
+                let data=await AssignmentModel.deleteOne({_id:obj._id});
+                console.log(data);
+                return data;
+          }catch(e){
+              throw e;
+          } 
+    },getAllTeacherAssignment:async(obj)=>{
+        try{
+         let data=await AssignmentModel.find({createdBy:obj.username});
+            return data;
+        }catch(e){
+            throw e;
+        }
+    }
    }
--- graphQL/schema.js
+++ graphQL/schema.js
@@ -2,7 +2,10 @@ var { graphql, buildSchema } = require('graphql');
 const utility=require('../DB/utility');
 let schema = buildSchema(`
   type Query {
-        createAssignment:Assignment
+        createAssignment(description:String!,deadLine:String!,createdBy:String!,students:[String]!):Assignment
+        updateAssignment(_id:String!,description:String,deadLine:String,createdBy:String,students:[String]):Assignment
+        deleteAssignment(_id:String!):Message
+        getAssignment(username:String,isStudent:Boolean):[Assignment]
     }
    type Assignment{
       description:String
@@ -10,14 +13,58 @@ let schema = buildSchema(`
       postedAt:String
       deadLine:String
       createdBy:String 
+      status:String
+      _id:String
    } 
+
+   type Message{
+       message:String
+   }
 
 `);
 
 let resolver = { 
-                    createAssignment:()=>{
-                            return "hello"
-                    }
+                     createAssignment:async (args)=>{
+                        try{
+                            args.postedAt=new Date();
+                            args.deadLine=new Date();
+                        let data=await utility.insertAssignment(args);
+                        return data;
+                        }catch(e){
+                            console.log(e);
+                            throw e;
+                        }
+                    },
+                    updateAssignment:async (args)=>{
+                        try{
+                        let data=await utility.updateAssignment(args);
+                        return data;
+                        }catch(e){
+                            throw e;
+                        }
+                    },
+                    deleteAssignment: async(args)=>{
+                        try{
+                            let data=await utility.deleteAssignment(args);
+                            if(data.deletedCount==1)
+                                    return {message:"Sucessfully Deleted"}
+                            return {message:"Invalid ID"};
+                        }catch(e){
+                            throw e;
+                        }
+                    },
+                    getAssignment:async (args)=>{
+                            if(args.isStudent){
+
+                            }else{
+                              try{
+                                     let data=await utility.getAllTeacherAssignment(args);
+                                     return data;
+                                }catch(e){
+                                    throw e;
+                                }
+                            }
+                  }
                 };
 module.exports={
     schema:schema,
