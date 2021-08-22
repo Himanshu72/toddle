86d7c4d7ffe07f28a4c5dd837ab6ad6f8aa24fdb
Himanshu_Joshii <202012094@daiict.ac.in>
2021-08-22 17:10:50 +0530

    final commit

--- index.js
+++ index.js
@@ -3,11 +3,39 @@ var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
 const resolver=require("./resolver");
 const jwt = require("./jwt");
+
 var schema = buildSchema(`
 type Query {
     createuser(username:String!,password:String!,isStudent:Boolean!):user 
     getuser(username:String!,password:String!):user
+    createAssignment(token:String!,username:String!,publishAt:String!,description:String!,deadLine:String!,createdBy:String!,students:[String]!):Assignment
+    updateAssignment(token:String!,username:String!,_id:String!,description:String,deadLine:String,createdBy:String,students:[String]):Assignment
+    deleteAssignment(token:String!,username:String!,_id:String!):Message
+    submitAssignment(token:String!,username:String!,remark:String!,submitedBy:String!,Assignment:String!):Submit
+    getAssignment(token:String!,username:String!,filter:String,username:String!,isStudent:Boolean!):[Assignment]
+    getAssignmentSubmission(token:String!,username:String!,isStudent:Boolean!,username:String!,Assignment:String!):[Submit]
   }
+
+  type Assignment{
+    description:String
+    students:[String]
+    publishAt:String
+    deadLine:String
+    createdBy:String 
+    status:String
+    _id:String
+ } 
+
+ type Submit{
+  _id:String
+  remark:String
+  submitedBy:String
+  Assignment:String 
+  }
+
+ type Message{
+     message:String
+ }
 
   type user{
       username:String   
@@ -18,6 +46,8 @@ type Query {
   
 `);
 
+
+let cache={};
 var root = { 
         createuser:async(args)=>{
             let data;   
@@ -30,22 +60,170 @@ var root = {
               console.log(e);
                 throw new Error("Invalid Input");
             }
-            
+            cache[data.username]=data;
             return data;
         },
         async getuser(args){
             try{
              let data= await resolver.getuser(args);
+             cache[data.username]=data;
+            
              return data; 
             }catch(e){
             
               throw e;
             }
+        },
+        createAssignment:async (args)=>{
+          try{    
+           
+            
+            
+          if(jwt.isAuth(args.username,args.token)==false){
+                    throw new Error("Invalid Token");
+              }else{
+                  if(cache[args.username].isStudent ){
+                    throw new Error("Access Denied...");
+                  } else{
+
+                    let data = await resolver.createAssignment(args);
+                       return data;   
+                  }  
+
+              }
+        } 
+        catch(e)
+        {
+          throw e;
         }
+      },
+      async updateAssignment(args){
+        try{    
+          
+        
+
+          if(jwt.isAuth(args.username,args.token)==false){
+                    throw new Error("Invalid Token");
+              }else{
+                  if(cache[args.username].isStudent ){
+                    throw new Error("Access Denied...");
+                  } else{
+
+                    let data = await resolver.updateAssignment(args);
+                       return data;   
+                  }  
+
+              }
+        } 
+        catch(e)
+        {
+          throw e;
+        }
+      },
+      async deleteAssignment(args){
+        try{    
+             
+          if(jwt.isAuth(args.username,args.token)==false){
+                    throw new Error("Invalid Token");
+              }else{
+                  if(cache[args.username].isStudent ){
+                    throw new Error("Access Denied...");
+                  } else{
+
+                    let data = await resolver.deleteAssignment(args);
+                       return data;   
+                  }  
+
+              }
+        } 
+        catch(e)
+        {
+          throw e;
+        }
+      },
+      async submitAssignment(args){
+        try{    
+             
+          if(jwt.isAuth(args.username,args.token)==false){
+                    throw new Error("Invalid Token");
+              }else{
+                  if(cache[args.username].isStudent ){
+                    throw new Error("Access Denied...");
+                  } else{
+
+                    let data = await resolver.deleteAssignment(args);
+                       return data;   
+                  }  
+
+              }
+        } 
+        catch(e)
+        {
+          throw e;
+        }
+      },
+      async submitAssignment(args){
+        try{    
+          
+          if(jwt.isAuth(args.username,args.token)==false){
+                    throw new Error("Invalid Token");
+              }else{
+                  if(!cache[args.username].isStudent ){
+                    throw new Error("Access Denied...");
+                  } else{
+                    let data = await resolver.submitAssignment(args);
+                       return data;   
+                  }  
+
+              }
+        } 
+        catch(e)
+        {
+          throw e;
+        }
+      },
+      async getAssignmentSubmission(args){
+        try{    
+            
+          if(jwt.isAuth(args.username,args.token)==false){
+                    throw new Error("Invalid Token");
+              }else{
+                    args.isStudent=cache[args.username].isStudent;        
+                    let data = await resolver.getAssignmentSubmission(args);
+                       return data;   
+                  
+              }
+        } 
+        catch(e)
+        {
+          throw e;
+        }
+      },
+      async  getAssignment(args){
+        try{    
+          let data = await resolver.getAssignment(args,args.filter);
+                       return data;  
+          if(jwt.isAuth(args.username,args.token)==false){
+                    throw new Error("Invalid Token");
+              }else{
+                    args.isStudent=cache[args.username].isStudent;        
+                    let data = await resolver.getAssignment(args,args.filter);
+                       return data;   
+                  
+              }
+        } 
+        catch(e)
+        {
+          throw e;
+        }
+               
+      }
+
 };
 
 var app = express();
-var cors = require('cors')
+var cors = require('cors');
+const { isAuth } = require('./jwt');
 app.use(cors())
 app.use('/graphql', graphqlHTTP({
   schema: schema,
--- resolver.js
+++ resolver.js
@@ -59,5 +59,237 @@ module.exports={
        }
         
         return result.data.data.getUser;
+    },
+    async createAssignment(args){
+      console.log(args)
+      let result;
+      let arr=args.students;
+      args.students="[";
+      for(let a of arr){
+        args.students+='"';
+        args.students+=a;
+        args.students+='"';
+        args.students+=',';      
+        }
+        args.students= args.students.substr(0,args.students.length-1)+  ']';
+        console.log(args.students);
+        try{
+         result=await axios({
+            url: 'http://localhost:4001/graphql',
+            method: 'post',
+            data: {
+              query: `
+                   
+                query {
+                  createAssignment(createdBy:"${args.createdBy}",deadLine:"${args.deadLine}",publishAt:"${args.publishAt}",students:${args.students},description:"${args.description}" ){
+                    createdBy
+                    deadLine
+                    students
+                  publishAt
+                  status
+                  description 
+                  _id
+              }	
+                }
+                `
+            }
+          })
+        }
+        catch(e){
+          //  console.log(e);
+            throw e;
+        }
+       if(result.data.errors){
+          throw new Error(result.data.errors[0].message);
+        }
+          
+        return result.data.data.createAssignment;
+    },
+    async updateAssignment(args){
+      console.log(args)
+      let result;
+      let arr=args.students;
+      args.students="[";
+      for(let a of arr){
+        args.students+='"';
+        args.students+=a;
+        args.students+='"';
+        args.students+=',';      
+        }
+        args.students= args.students.substr(0,args.students.length-1)+  ']';
+        console.log(args.students);
+        try{
+         result=await axios({
+            url: 'http://localhost:4001/graphql',
+            method: 'post',
+            data: {
+              query: `
+                   
+                query {
+                  updateAssignment(_id:"${args._id}",createdBy:"${args.createdBy}",deadLine:"${args.deadLine}",students:${args.students},description:"${args.description}" ){
+                    createdBy
+                    deadLine
+                    students
+                  publishAt
+                  status
+                  description 
+                  _id
+              }	
+                }
+                `
+            }
+          })
+        }
+        catch(e){
+          //  console.log(e);
+            throw e;
+        }
+       if(result.data.errors){
+          throw new Error(result.data.errors[0].message);
+        }
+          
+        return result.data.data.updateAssignment;
+    },
+    async deleteAssignment(args){
+      try{
+        result=await axios({
+           url: 'http://localhost:4001/graphql',
+           method: 'post',
+           data: {
+             query: `
+                  
+               query {
+                 deleteAssignment(_id:"${args._id}" ){
+                   message
+             }	
+               }
+               `
+           }
+         })
+       }
+       catch(e){
+         //  console.log(e);
+           throw e;
+       }
+      if(result.data.errors){
+         throw new Error(result.data.errors[0].message);
+       }
+         
+       return result.data.data.deleteAssignment;
+    },
+    async submitAssignment(args){
+      try{
+        result=await axios({
+           url: 'http://localhost:4001/graphql',
+           method: 'post',
+           data: {
+             query: `
+                  
+               query {
+                submitAssignment(submitedBy:"${args.submitedBy}",Assignment:"${args.Assignment}",remark:"${args.remark}" ){
+                  submitedBy,
+                  Assignment,
+                  remark,
+                  _id
+                }
+               }
+               `
+           }
+         })
+       }
+       catch(e){
+         //  console.log(e);
+           throw e;
+       }
+      if(result.data.errors){
+         throw new Error(result.data.errors[0].message);
+       }
+         
+       return result.data.data.submitAssignment;
+    }
+    ,
+    async getAssignmentSubmission(args){
+      try{
+        result=await axios({
+           url: 'http://localhost:4001/graphql',
+           method: 'post',
+           data: {
+             query: `
+                  
+               query {
+                getAssignmentSubmission(isStudent:${args.isStudent},username:"${args.username}",Assignment:"${args.Assignment}" ){
+                  submitedBy,
+                  _id,
+                  remark
+                  Assignment	
+              }
+               }
+               `
+           }
+         })
+       }
+       catch(e){
+         //  console.log(e);
+           throw e;
+       }
+      if(result.data.errors){
+         throw new Error(result.data.errors[0].message);
+       }
+         
+       return result.data.data.getAssignmentSubmission;
+    },
+    async getAssignment(args,filter){
+      try{
+        
+      if(!filter){
+        result=await axios({
+           url: 'http://localhost:4001/graphql',
+           method: 'post',
+           data: {
+             query: `
+                  
+               query {
+                getAssignment(username:"${args.username}",isStudent:${args.isStudent}){
+                  status
+                  deadLine
+                  description
+                  publishAt
+                  students
+                }
+              }
+               `
+           }
+         })
+        }else{
+          result=await axios({
+            url: 'http://localhost:4001/graphql',
+            method: 'post',
+            data: {
+              query: `
+                   
+                query {
+                 getAssignment(username:"${args.username}",isStudent:${args.isStudent},filter:${args.filter}){
+                   status
+                   deadLine
+                   description
+                   publishAt
+                   students
+                 }
+               }
+                `
+            }
+          })
+           
+        }
+       }
+       catch(e){
+         //  console.log(e);
+           throw e;
+       }
+      if(result.data.errors){
+         throw new Error(result.data.errors[0].message);
+       }
+         
+       return result.data.data.getAssignment;
     }
 };
